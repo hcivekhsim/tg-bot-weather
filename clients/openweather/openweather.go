078595e8089d9f7ef321de2710a01ea5e0dc1d49
Максим <mmish0829j@gmail.com>
2026-02-12 20:33:11 +0300

    fix: url

---
+++ clients/openweather/openweather.go
@@ -0,0 +1,68 @@
+package openweather
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type OpenWeatherClient struct {
+	api_key string
+}
+
+func New(api_key string) *OpenWeatherClient {
+	return &OpenWeatherClient{
+		api_key: api_key,
+	}
+}
+
+func (o OpenWeatherClient) Coordinates(city string) (Coordinates, error) {
+	url := "http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=5&appid=%s"
+
+	resp, err := http.Get(fmt.Sprintf(url, city, o.api_key))
+	if err != nil {
+		return Coordinates{}, fmt.Errorf("error get coordinates: %w", err)
+	}
+	if resp.StatusCode != 200 {
+		return Coordinates{}, fmt.Errorf("error fail get coordinates: %d", resp.StatusCode)
+	}
+
+	var coordinatesResponse []CoordinatesResponse
+	err = json.NewDecoder(resp.Body).Decode(&coordinatesResponse)
+	if err != nil {
+		return Coordinates{}, fmt.Errorf("error unmarshal response: %w", err)
+	}
+
+	if len(coordinatesResponse) == 0 {
+		return Coordinates{}, fmt.Errorf("error empty coordinates")
+	}
+
+	return Coordinates{
+		Lat: coordinatesResponse[0].Lat,
+		Lon: coordinatesResponse[0].Lon,
+	}, nil
+
+}
+
+func (o OpenWeatherClient) Weather(lat, lon float64) (Weather, error) {
+	url := "https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s&units=metric"
+	resp, err := http.Get(fmt.Sprintf(url, lat, lon, o.api_key))
+	if err != nil {
+		return Weather{}, fmt.Errorf("error get weather: %w", err)
+	}
+
+	if resp.StatusCode != 200 {
+		return Weather{}, fmt.Errorf("error fail get weather: %d", resp.StatusCode)
+	}
+
+	var weatherResponse WeatherResponse
+	err = json.NewDecoder(resp.Body).Decode(&weatherResponse)
+
+	if err != nil {
+		return Weather{}, fmt.Errorf("error unmarshal weather response: %w", err)
+	}
+
+	return Weather{
+		Temp: weatherResponse.Main.Temp,
+	}, nil
+}
---
+++ handler/handler.go
@@ -0,0 +1,64 @@
+package handler
+
+import (
+	"fmt"
+	"log"
+	"math"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/hcivekhsim/tg-bot-weather/clients/openweather"
+)
+
+type Handler struct {
+	bot      *tgbotapi.BotAPI
+	owClient *openweather.OpenWeatherClient
+}
+
+func New(bot *tgbotapi.BotAPI, owClient *openweather.OpenWeatherClient) *Handler {
+	return &Handler{
+		bot:      bot,
+		owClient: owClient,
+	}
+}
+func (h *Handler) Start() {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := h.bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		h.handleUpdate(update)
+	}
+}
+func (h *Handler) handleUpdate(update tgbotapi.Update) {
+	if update.Message == nil {
+		return
+	}
+
+	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+	coordinates, err := h.owClient.Coordinates(update.Message.Text)
+	if err != nil {
+		log.Println(err)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Не смог получить координаты :(")
+		msg.ReplyToMessageID = update.Message.MessageID
+		h.bot.Send(msg)
+		return
+	}
+
+	weather, err := h.owClient.Weather(coordinates.Lat, coordinates.Lon)
+	if err != nil {
+		log.Println(err)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Не смог получить погоду :(")
+		msg.ReplyToMessageID = update.Message.MessageID
+		h.bot.Send(msg)
+		return
+	}
+
+	msg := tgbotapi.NewMessage(
+		update.Message.Chat.ID,
+		fmt.Sprintf("➡ Температура в %s: %d°C", update.Message.Text, int(math.Round(weather.Temp))),
+	)
+	msg.ReplyToMessageID = update.Message.MessageID
+
+	h.bot.Send(msg)
+}
